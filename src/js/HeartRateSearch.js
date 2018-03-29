dfc43fa5797254117c2ced7503a9fc291a1e05f4
anika-mukherji <anika.mukherji@duke.edu>
2018-03-28 22:32:22 -0400

    sucessfully calling server

--- src/js/HeartRateSearch.js
+++ src/js/HeartRateSearch.js
@@ -6,6 +6,15 @@ import axios from 'axios'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import {cyan500} from 'material-ui/styles/colors';
+
+const muiThemeWhiteText = getMuiTheme({
+  textField: {
+    hintColor: 'white',
+    labelColor: 'white',
+    floatingLabelColor: cyan500,
+  },
+});
 
 
 
@@ -14,28 +23,40 @@ class HeartRateSearch extends Component {
   constructor() {
     super();
     this.state = {
-      "curr_user": "None",
+      "curr_user": null,
+      "user_hr": [null],
       "graph_visible": false,
     };
+  }
+
+  getHR = () => {
+    axios.get("http://vcm-3576.vm.duke.edu:5000/api/heart_rate/anika@anika.com").then( (response) => { 
+      console.log(response.status);
+      this.setState({"user_hr": response.data});
+    }); 
   }
 
   render() {
     return (
       <div className="hrsearch-body">
-        <div className="email-field">
-          <TextField
-            hintText="Who Are You?"
-            floatingLabelText="Email"
-            type="Email" />
-        </div>
-      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-        <div className="email-search-button">
-          <RaisedButton 
-            primary={true}
-            label="Find Heart Rate"
-            labelColor="white" />
-        </div>
-      </MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiThemeWhiteText}>
+          <div className="email-field">
+            <TextField
+              hintText="Who Are You?"
+              floatingLabelText="Email"
+              type="Email" 
+              ref="textField" />
+          </div>
+        </MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <div className="email-search-button">
+            <RaisedButton 
+              primary={true}
+              label="Find Heart Rate"
+              labelColor="white" 
+              onClick={this.getHR}/>
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
